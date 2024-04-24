4ab3c32678159456d8aaf8d46e380bfbd408d028
Karthik <paulatreides@duck.com>
2024-04-24 16:30:32 +0530

    Getting config from user

--- cmd/projector/main.go
+++ cmd/projector/main.go
@@ -12,4 +12,9 @@ func main() {
     if err != nil {
         log.Fatalf("Unable to get options %v", err)
     }
+
+    conf, err := config.NewConfig(opts)
+    if err != nil {
+        log.Fatalf("Unable to get config %v", err)
+    }
 }
